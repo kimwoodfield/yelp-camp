e9fffe32f884fe776320bec543171cb387a0d2ac
kimwoodfield <kimwoodfield@live.com>
2021-07-06 08:46:48 +1000

    display all campgrounds route added

--- app.js
+++ app.js
@@ -23,15 +23,12 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-app.get("/makeCampground", async (req, res) => {
-  const camp = new Campground({
-    title: "My backyard",
-    description: "Cheap camping!",
-  });
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds })
 });
+
+
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000!`);
