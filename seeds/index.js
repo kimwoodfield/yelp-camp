ce765c0baf6b629e00202eaff0dbb048eae1c14f
kimwoodfield <kimwoodfield@live.com>
2021-08-01 13:05:50 +1000

    add basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 1;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, ducimus numquam, fugiat corrupti odio error voluptatum quidem dolorem eligendi facere velit nulla necessitatibus quas molestias dolor minus eum tenetur beatae.',
+      price
     });
     await camp.save();
   }
